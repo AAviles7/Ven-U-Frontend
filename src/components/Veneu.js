b0c54b33e546290d64bc6c84a5e27c87fb1f5f39
nick_baccete <nickbaccete@gmail.com>
2021-03-29 15:59:44 -0400

    git hub issues resolved

--- src/components/Veneu.js
+++ src/components/Veneu.js
@@ -4,7 +4,7 @@ const Venue = ({venues}) => {
   return(
     <div class='venues'>
       <div class='header'>
-        <h1>Venues</h1>
+        <h1 className='text-center' >Venues</h1><br></br>
       </div>
       <div>
           {venues.map(venue => <p>{'Address: '+venue.address}</p>)}
