e3b2dc791b7a339553520b95db2f8c1b754fd236
nick_baccete <nickbaccete@gmail.com>
2021-03-29 18:28:04 -0400

    finished VenueDetail skeleton

--- src/components/VenueDetails.js
+++ src/components/VenueDetails.js
@@ -1,39 +1,26 @@
 import React from 'react'
-import { Item, Image } from 'semantic-ui-react'
+import { Item, Image, Divider } from 'semantic-ui-react'
 
 const VenueDetails = ({venue}) => {
 
   return(
 
     <Item.Group>
+      <Divider hidden />
     <Item>
       <Item.Image size='tiny' src='/images/wireframe/image.png' />
-
+      
       <Item.Content>
         <Item.Header>{venue.name}</Item.Header>
         <Item.Meta>
           <span className='address'> {venue.address} </span><br></br>
-          <span className='stay'>1 Month</span>
+          <span className='price'>$1200</span>
         </Item.Meta>
         <Item.Description>hello hello hello hello</Item.Description>
       </Item.Content>
-    </Item>
 
-    {/* <Item>
-      <Item.Image size='tiny' src='/images/wireframe/image.png' />
-      
-      <Item.Content>
-      <Item.Header>Buck's Homebrew Stayaway</Item.Header>
-      <Item.Meta content='$1000 2 Weeks' />
-      <Item.Description>{paragraph}</Item.Description>
-      </Item.Content>
-      </Item>
-      
-      <Item>
-      <Item.Image size='tiny' src='/images/wireframe/image.png' />
-      <Item.Content header='Arrowhead Valley Camp' meta='$1200 1 Month' />
-    </Item> */}
-  </Item.Group>
+    </Item>
+    </Item.Group>
   
   )
 
