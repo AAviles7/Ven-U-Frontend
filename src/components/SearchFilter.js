21201873badcdebb90d82f7262143b4929f47b53
Abraham Aviles <aaaviles1995@gmail.com>
2021-03-30 14:32:40 -0400

    eventPage

--- src/components/SearchFilter.js
+++ src/components/SearchFilter.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Search, Checkbox, Container, Divider, Select } from 'semantic-ui-react'
 
 const SearchFilter = ({  }) => {
+    let sortOptions = [
+        {value: '', text: 'Show All'},
+        {value: 'LtoH', text: 'price low to high'},
+        {value: 'HtoL', text: 'price high to low'},
+        {value: 'AtoZ', text: 'alphabetical A to Z'},
+        {value: 'ZtoA', text: 'alphabetical Z to A'},
+        {value: 'age', text: 'age restriction'},
+        {value: 'stat', text: 'status'},
+    ]
     return(
-        <div>Search/Filter Component</div>
+        <Container>
+            <Search />
+            <Divider />
+            <Select placeholder='Select Sort By' options={sortOptions}/>
+        </Container>
     )
 }
 
