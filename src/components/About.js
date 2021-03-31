a7572210a1ac83613458368e9ceef0664e8c8b6e
Yvonne Chen <yvonnechen1991@gmail.com>
2021-03-31 12:48:19 -0500

    add more in about page

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ const About = () => {
             <h1>Welcome to Ven-U!</h1>
             <br></br>
             <p>
-                This is a Venue web app just for you! As an user, you are able to choose your favorite venue to host your event. You can also purchase a ticket to attend your favorite artist's event. 
+                This is a Venue web app just for you! As an user, you are able to choose your favorite venue to host your event. You can also purchase a ticket to attend your favorite artist's event. Hope you have a great journey using our app and remember to come back to visit us again! :)
             </p>
             <br></br>
             <img src={logo}></img>
