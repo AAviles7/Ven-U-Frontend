313d40e466f046ba41d7246a7877ebbaf0f27d1f
Yvonne Chen <yvonnechen1991@gmail.com>
2021-03-31 11:44:56 -0500

    logo

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,15 @@
+import logo from "../img/logo.png";
+
 const About = () => {
     return (
         <div className="text-center">
             <h1>Welcome to Ven-U!</h1>
+            <br></br>
             <p>
                 This is a Venue web app just for you! As an user, you are able to choose your favorite venue to host your event. You can also purchase a ticket to attend your favorite artist's event. 
             </p>
+            <br></br>
+            <img src={logo}></img>
         </div>
     )
 }
