fddad1e2072f03b0d8ed891f8cc909376c80d886
Yvonne Chen <yvonnechen1991@gmail.com>
2021-03-30 23:47:09 -0500

    about page

--- src/components/About.js
+++ src/components/About.js
@@ -2,6 +2,9 @@ const About = () => {
     return (
         <div className="text-center">
             <h1>Welcome to Ven-U!</h1>
+            <p>
+                This is a Venue web app just for you! As an user, you are able to choose your favorite venue to host your event. You can also purchase a ticket to attend your favorite artist's event. 
+            </p>
         </div>
     )
 }
